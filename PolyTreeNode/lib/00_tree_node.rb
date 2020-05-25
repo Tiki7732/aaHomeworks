d39aa8762d131bdaa3cd8f74066678bddb0f537d
Laurent Saidah <lsaidah@gmail.com>
2020-05-25 18:15:39 -0500

    adding the attr accessor and reader

--- PolyTreeNode/lib/00_tree_node.rb
+++ PolyTreeNode/lib/00_tree_node.rb
@@ -1,9 +1,10 @@
 class PolyTreeNode
 
-    attr_reader :value
-    def intialize(value)
-        parent = nil
-        children = []
+    attr_reader :value, :children
+    attr_accessor :parent
+    def initialize(value)
+        @parent = nil
+        @children = []
         @value = value
     end
 end
